2bc518ee6ae3f96b4b6fca52f66946f92b0b3d24
Vasi <vasiletanasa@gmail.com>
2025-05-24 14:13:50 +0300

    🔁 Upload agent Smart Tweet Responder

---
+++ test_summary.py
@@ -0,0 +1,9 @@
+from generate_summary import generate_summary
+
+tweets = [
+    "The future of Europe relies on rational leadership.",
+    "A mathematician running for president brings clarity and vision.",
+    "We need leaders who use logic, not emotion."
+]
+
+print(generate_summary(tweets))
---
+++ urls_data.py
@@ -0,0 +1,10 @@
+# urls_data.py
+
+
+MONITORED_URLS = [
+    "https://twitter.com/Lica2216",
+    "https://x.com/MihaelaIlie17"
+]
+
+
+
