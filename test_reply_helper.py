c1a22be1404ea6f2903bd860b437f1a0e7b1f234
Vasi <vasiletanasa@gmail.com>
2025-05-24 14:13:50 +0300

    🔁 Upload agent Smart Tweet Responder

---
+++ test_reply_helper.py
@@ -0,0 +1,6 @@
+from reply_helper import launch_reply
+
+url = "https://twitter.com/Lica2216/status/1789876543212345678"
+reply = "Awesome insights, thanks for sharing! 🔥"
+
+launch_reply(url, reply)
