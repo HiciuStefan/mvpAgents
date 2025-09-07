db0fd828c515a58b3bfd866ae8f79fa8ae0f8b6b
itoutcome <alex@itoutcome.com>
2025-09-08 01:13:17 +0300

    add middleware for clerk

--- dashboard/src/middleware.ts
+++ dashboard/src/middleware.ts
@@ -1,22 +1,55 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
+// import { ROUTES } from './lib/nav_items';
 
 const isPublicRoute = createRouteMatcher([
   '/',
   '/sign-in(.*)',
-  '/sign-up(.*)'
-])
+  '/sign-up(.*)',
+  '/api/webhook/clerk(.*)',
+]);
+
+// Define onboarding routes that should be accessible during onboarding
+// const isOnboardingRoute = createRouteMatcher(['/onboarding(.*)']);
 
 export default clerkMiddleware(async (auth, req) => {
-  // if (!isPublicRoute(req)) {
-  //   await auth.protect()
+  const {
+    pathname,
+    // origin
+  } = req.nextUrl;
+
+  if (!isPublicRoute(req)) {
+    await auth.protect();
+  }
+
+  if (
+    pathname.startsWith('/api') ||
+    pathname.startsWith('/trpc') ||
+    pathname.startsWith('/api/trpc')
+  ) {
+    return NextResponse.next();
+  }
+
+  // const { orgId } = await auth();
+  // // redirect to onboardingselection page when user has no active organization and is not on onboarding route
+  // if (!orgId && !isPublicRoute(req) && !isOnboardingRoute(req)) {
+  //   const selectionUrl = new URL(ROUTES.ONBOARDING.SELECTION, origin);
+  //   selectionUrl.searchParams.set('redirectTo', pathname);
+  //   return NextResponse.redirect(selectionUrl);
   // }
-})
+});
+
+// export default clerkMiddleware(async (auth, req) => {
+//   // if (!isPublicRoute(req)) {
+//   //   await auth.protect()
+//   // }
+// });
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    '/((?!_next|[^?]*\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
-    "/(api|trpc)(.*)",
+    '/(api|trpc)(.*)',
   ],
 };
